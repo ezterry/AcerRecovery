0f6c30586a0c36caea2f51eecf767b6bf41ff729
Terrence Ezrol <ezterry3@gmail.com>
2012-05-20 21:09:00 -0400

    Do not erase /data/linux
    For chrooting and dual booting linux users do not:
    A) erase /data/linux on wipe user data
    B) backup /data/linux as part of the userdata backup
    
    (Note if you have an older nandroid with /data/linux already backed up
     restoring it may overwrite some updated files)

--- extendedcommands.c
+++ extendedcommands.c
@@ -557,7 +557,7 @@ int format_unknown_device(const char *device, const char* path, const char *fs_t
 
     static char tmp[PATH_MAX];
     if (strcmp(path, "/data") == 0) {
-        sprintf(tmp, "cd /data ; for f in $(ls -a | grep -v ^media$); do rm -rf $f; done");
+        sprintf(tmp, "cd /data ; for f in $(ls -a | grep -v ^media$ | grep -v ^linux); do rm -rf $f; done");
         __system(tmp);
     }
     else {
